de9dba5bcdd1e1e2b45e5f18e5ba8fa1e5c76dcb
RishinR <rishinrnew2020@gmail.com>
2023-05-27 20:43:32 +0530

    reverse a linkedlist using the 3 pointer method

--- reversealinkedlist.py
+++ reversealinkedlist.py
@@ -1,43 +1,30 @@
-class Node:
-    def __init__(self, data = None, next = None):
-        self.data = data
-        self.next = next
-class LinkedList:
-    def __init__(self):
-        self.head = None
-    def insert(self, data):
-        if self.head is None:
-            self.head = Node(data, None)
-        else:
-            itr = self.head
-            while itr.next:
-                itr = itr.next
-            itr.next = Node(data, None)
-    def print(self):
-        itr = self.head
-        lisstr = ""
-        while itr:
-            lisstr += str(itr.data) + "-->"
-            itr = itr.next
-        print(lisstr)
-    def reverse(self):
-        if self.head is None:
-            return self.head
-        else:
-            prev = None
-            curr = self.head
-            
-        while curr:
-            next_node = curr.next # next element after head
-            curr.next = prev # head.next = None
-            prev = curr # setting new prev as current value fro next iteration
-            curr = next_node # setting current as next_node  
-        self.head = prev      
-if __name__ == "__main__":
-    l = LinkedList()
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        if head == None:
+            return None
+        itr1 = head
+        if head.next == None:
+            return head
+        itr2 = head.next
+        if head.next.next == None:
+            itr2.next = itr1
+            itr1.next = None
+            head = itr2
+            return head
+        itr3 = head.next.next
 
-    for i in range(0, 5):
-        n = int(input("Enter a number: "))
-        l.insert(n)
-    l.reverse()
-    l.print()
+        while itr3:
+            if itr1 == head:
+                itr1.next = None
+            itr2.next = itr1
+            itr1 = itr2
+            itr2 = itr3
+            itr3 = itr3.next
+        itr2.next = itr1
+        head = itr2
+        return head
