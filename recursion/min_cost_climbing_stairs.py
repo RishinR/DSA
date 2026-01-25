201e5640bca95dcfa55d63322b3f66130b8a9a66
rishin <rishinrnew2020@gmail.com>
2026-01-25 19:52:01 +0530

    added min cost climbing stairs

---
+++ recursion/min_cost_climbing_stairs.py
@@ -0,0 +1,21 @@
+def main():    
+    def solve(index, dp):
+        if index >= len(cost):
+            return 0
+        
+        if dp[index] != -1:
+            return dp[index]
+
+        one = cost[index] + solve(index + 1, dp)
+        two = cost[index] + solve(index + 2, dp)
+        
+        dp[index] = min(one, two)
+        return dp[index]
+    
+    cost = [10,15,20]
+    dp = [-1 for _ in range(len(cost))]
+    result = min(solve(0, dp), solve(1, dp))
+    print(result)
+
+if __name__ == "__main__":
+    main()
