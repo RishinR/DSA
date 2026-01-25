44884f36a28d5f6374de3432f60c517a6032a8f8
rishin <rishinrnew2020@gmail.com>
2026-01-25 21:21:42 +0530

    reorganised tree problems

--- dp/house_robber.py
+++ dp/house_robber.py
@@ -1,21 +1,23 @@
 # leetcode 198
 
+
 def main():
     def solve(index, dp):
         if index >= len(nums):
             return 0
-        
+
         if dp[index] != -1:
             return dp[index]
-        
+
         rob_current = nums[index] + solve(index + 2, dp)
         skip_current = solve(index + 1, dp)
         return max(rob_current, skip_current)
 
-    nums = [1,2,3,1]
+    nums = [1, 2, 3, 1]
     dp = [-1 for _ in range(len(nums))]
     result = solve(0, dp)
     print(result)
+
 
 if __name__ == "__main__":
     main()
